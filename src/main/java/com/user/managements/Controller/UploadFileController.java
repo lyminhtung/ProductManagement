e0c7a31925c140ad794d65b2777e6b599513cf5f
lyminhtung <lyminhtung08@gmail.com>
2026-05-09 17:53:50 +0700

    Clean code by using lombok and update some feature

--- src/main/java/com/user/managements/Controller/UploadFileController.java
+++ src/main/java/com/user/managements/Controller/UploadFileController.java
@@ -2,33 +2,27 @@ package com.user.managements.Controller;
 
 import com.user.managements.Model.ResponseObject;
 import com.user.managements.Services.IStorageService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
-@Controller
+@RestController
 @RequestMapping(path = "api/v1/file")
+@RequiredArgsConstructor
 public class UploadFileController {
-    @Autowired
-    private IStorageService storageService;
+
+    private final IStorageService storageService;
+
     @PostMapping
-    public ResponseEntity<ResponseObject> uploadFile(@RequestParam("file")MultipartFile file){
-        try{
+    public ResponseEntity<ResponseObject> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
             String generatedFileName = storageService.storageFile(file);
-            return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject("ok","Uploaded file successfully!",generatedFileName)
-            );
-        }
-        catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
-                    new ResponseObject("error","Upload file failed!",e.getMessage())
-            );
+            return ResponseEntity.ok(new ResponseObject("ok", "Uploaded file successfully!", generatedFileName));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseObject("error", e.getMessage(), ""));
         }
     }
 }
--- src/main/java/com/user/managements/Repositories/ProductRepository.java
+++ src/main/java/com/user/managements/Repositories/ProductRepository.java
@@ -1,12 +1,8 @@
 package com.user.managements.Repositories;
 
-// Nơi để chúng ta chữa các hàm để lấy data về
-
 import com.user.managements.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-    List<Product> findByName(String name);
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByName(String name);
 }
